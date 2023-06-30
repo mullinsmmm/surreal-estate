11ccc66c60bb49274c9a908c336ccb01f37520ae
Matt Mullins <matt.mullins@live.com>
2023-06-30 22:19:50 +0100

    NavBar init

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import '../styles/App.css';
+import React from 'react';
+import NavBar from './NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Surreal Estate</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h1>Surreal Estate</h1>
+    <NavBar />
+  </div>
+);
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/NavBar.css';
+
+const NavBar = () => (
+  <div className="NavBar">
+    <p>NavBar div test</p>
+  </div>
+);
+
+export default NavBar;
